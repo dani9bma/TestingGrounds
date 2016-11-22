1cf87f3e37a883d6163679138bdd6bbb15e66354
Daniel Assuncao <dinho9.epaeh@hotmail.com>
2016-11-22 18:56:07 +0000

    Beginning of Cpp AI

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TestingGrounds.h"
+#include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory){
+
+	auto blackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = blackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+
+	return EBTNodeResult::Succeeded;
+}
